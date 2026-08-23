baa47de21c95c88fc4805ae55a145478bd70dfcb
karimosama99 <ko5418301@gmail.com>
2026-08-23 03:21:14 +0300

    hotFix

--- components/scrollbar/scrollbar.tsx
+++ components/scrollbar/scrollbar.tsx
@@ -1,11 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Scrollbar: React.FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener('scroll', toggleVisibility);
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
   return (
-    <ul className="smothscroll">
+    <ul className={`smothscroll ${isVisible ? 'active' : ''}`}>
       <li>
         <AnchorLink
           href="#scrool"
